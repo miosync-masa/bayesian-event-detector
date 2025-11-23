71120b1fa65177d3ab442a350e3148519c97b98a
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2025-11-23 17:49:58 +0900

    Initialize data_generation module with imports

---
+++ lambda3_benchmark/data_generation/__init__.py
@@ -0,0 +1,11 @@
+from .delayed_domino import (
+    DelayedDominoConfig,
+    generate_delayed_domino,
+    generate_dataset_batch
+)
+
+from .unidirectional import (
+    UnidirectionalDominoConfig,
+    generate_unidirectional_domino,
+    generate_unidirectional_batch
+)
